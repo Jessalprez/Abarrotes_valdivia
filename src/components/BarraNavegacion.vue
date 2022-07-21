75a0b7fd675cd714bb46bd0f446a3fdc71d8abac
Jessalprez <jessalprez1@hotmail.com>
2022-07-20 22:35:33 -0500

    Medio barra de navegación

---
+++ src/components/BarraNavegacion.vue
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<template>
+    <div class="barra">
+        <ul> 
+            <li class="texto">Inventario</li>
+            <li class="texto">Compra</li>
+            <li class="texto">Venta</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.barra{
+    background-color: black;
+    height: 100vh;
+    width: 15vw;
+}
+.texto{
+    color: rgb(255, 255, 255);
+    transition: all ease-out 0.5s;
+}
+.texto:hover{
+    color:red;
+}
+</style>
